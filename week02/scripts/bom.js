ba9cbb6dec1214d108b542398ed123ebbb72f08d
adelso.tzep <atzepchox@byupathway.edu>
2025-11-08 01:00:10 -0600

    Activities completed

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,31 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('_____');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌'
-li.append(deleteButton);
-list.append(li);
-<button aria-label="Close" id="close-button">❌</button>
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+
+    }
+
+    input.focus();
+});
 
 
